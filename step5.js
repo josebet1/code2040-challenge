1366acc030dc6e2f81ad640a35b9c658ac295ac0
Jose Bethancourt <jose@joseb.me>
2016-08-17 23:42:53 -0700

    step 5 complete

---
+++ step5.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+const authToken = '<authToken>';
+
+function returnTime(timeString) {
+  request.post({
+    uri: 'http://challenge.code2040.org/api/dating/validate',
+    method: 'POST',
+    json: { token: authToken, datestamp: timeString } },
+    (err, response, body) => {
+      if (!err && response.statusCode === 200) {
+        console.log(body);
+      }
+    });
+}
+
+request.post({
+  uri: 'http://challenge.code2040.org/api/dating',
+  method: 'POST',
+  json: { token: authToken } },
+  (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      const parsedTime = new Date(body.datestamp).getTime();
+      const finalTime = parsedTime + (body.interval * 1000);
+      returnTime(new Date(finalTime).toISOString().split('.')[0] + 'Z');
+    }
+  });
